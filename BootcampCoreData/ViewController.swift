f8caffc0f62431a15b7702e456cddbba93869b83
TechCampus <techcampus@razans-MacBook-Pro.local>
2019-01-24 00:38:22 +0200

    coredata practice

--- BootcampCoreData/ViewController.swift
+++ BootcampCoreData/ViewController.swift
@@ -7,14 +7,84 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
+    //MARK: - IBOutlets
+    @IBOutlet weak var tableView: UITableView!
+    
+    //MARK: - Private Variables
+    var personName: [NSManagedObject] = []
+    
+    //MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest <NSManagedObject>(entityName: "Person")
+        
+        do {
+            self.personName = try managedContext.fetch(fetchRequest)
+        } catch let error as NSError {
+            print("Couldn't retrive names \(error.userInfo)")
+        }
     }
 
-
+    @IBAction func btnAddNewPersonNameTapped(_ sender: Any) {
+        
+        let alert = UIAlertController(title: "New Name", message: "Add a new name", preferredStyle: .alert)
+        
+        let saveAction = UIAlertAction(title: "Save", style: .default) { [unowned self] action in
+            guard let textField = alert.textFields?.first,
+                let nameToSave = textField.text else {
+                    return
+            }
+            
+            guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+                return
+            }
+            
+            let managedContext = appDelegate.persistentContainer.viewContext
+            
+            let entity = NSEntityDescription.entity(forEntityName: "Person", in: managedContext)!
+            let person = NSManagedObject(entity: entity, insertInto: managedContext)
+            person.setValue(nameToSave, forKey: "name")
+            
+            do {
+                try managedContext.save()
+                self.personName.append(person)
+            } catch let error as NSError {
+                print("couldn't save name \(error)")
+            }
+            self.tableView.reloadData()
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .default, handler: nil)
+        alert.addTextField()
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        present(alert, animated: true, completion: nil)
+    }
+    
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.personName.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let person = personName[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = person.value(forKey: "name") as? String
+        return cell
+    }
+}
